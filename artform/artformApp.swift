cef3e9437539a47519893715c9583aac77604f0d
Vedanshi <vedantya738@gmail.com>
2026-02-05 13:58:37 +0530

    Initial Commit

---
+++ artform/artformApp.swift
@@ -0,0 +1,17 @@
+//
+//  artformApp.swift
+//  artform
+//
+//  Created by Vedanshi Prajapati on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct artformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ artformTests/artformTests.swift
@@ -0,0 +1,17 @@
+//
+//  artformTests.swift
+//  artformTests
+//
+//  Created by Vedanshi Prajapati on 05/02/26.
+//
+
+import Testing
+@testable import artform
+
+struct artformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

f15f458a47fe74a5b1eb7e8a82e78d4721b0568d
Vedanshi <vedantya738@gmail.com>
2026-02-17 09:05:25 +0530

    buttons, tabs fixed

--- artform/artform-Bridging-Header.h
+++ artform/artform-Bridging-Header.h
@@ -1,4 +1,7 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  artform-Bridging-Header.swift
+//  artform
+//
+//  Created by Vedanshi Prajapati on 16/02/26.
 //
 
